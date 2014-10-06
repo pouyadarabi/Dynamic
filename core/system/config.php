049357740bd801c9c7ea72216a4968e93e9a7c50
PouyaDarabi <pouyyadarabi@gmail.com>
2014-10-06 16:37:04 +0330

    update config

--- core/system/config.php
+++ core/system/config.php
@@ -1,41 +1,37 @@
 <?php
-
-return array(
-
-    // ------------------------| Config |--------------------------- #
-    'DebugMode'  => TRUE,
-    'SqlErrorDetais' => false,
-
-    // --------------------------| DB |----------------------------- #
-    'DbName' => 'DB_NAME', // Db Name
-    'DbHost' => 'DB_HOST', // Db Host
-    'DbUser' => 'DB_USER', // Db UserName
-    'DbPass' => 'DB_PASS', // Db PassWord
-
-    // ----------------------| Security |----------------------- #
-    'Blowfish_Pre' => '$6$rounds=>5000$', // blowfish for CRYPT_SHA256 encryption (php.net/manual/en/function.crypt.php)
-    'Blowfish_End' => '$', // blowfish for encryption
-    'UrlAllowedChars' => 'a-z 0-9~%.:_\-=&', // Allowed chars in url
-    'Session_IPCheck' => FALSE,
-    'Session_UserAgentCheck' => FALSE,
-    'Session_Secure' => TRUE, // check user agent and ip
-
-    // ----------------------| Config |----------------------- #
-
-    'AppName' => 'Dynamic', // application name (used in cookie name)
-
-    // Whilte List Controllers
-
-    'Controllers' => array('main'),
-
-
-    /// Method Permissions
-
-    'CheckPermissions' => TRUE ,
-    'ForbiddenByDefault' => TRUE,
-    'AntiCsrf' => TRUE,
-    'CsrfTokenName' => '__pctk',
-	'DefualtPerm' => 'guest'
-
-
+return array (
+		
+		// ------------------------| Config |--------------------------- #
+		'DebugMode' => TRUE,
+		'SqlErrorDetais' => FALSE,
+		
+		// --------------------------| DB |----------------------------- #
+		'DbName' => 'DB_NAME', // Db Name
+		'DbHost' => 'DB_HOST', // Db Host
+		'DbUser' => 'DB_USER', // Db UserName
+		'DbPass' => 'DB_PASS', // Db PassWord
+		                       
+		// ----------------------| Security |----------------------- #
+		'Blowfish_Pre' => '$6$rounds=>5000$', // blowfish for CRYPT_SHA256 encryption (php.net/manual/en/function.crypt.php)
+		'Blowfish_End' => '$', // blowfish for encryption
+		'UrlAllowedChars' => 'a-z 0-9~%.:_\-=&', // Allowed chars in url
+		'Session_IPCheck' => FALSE,
+		'Session_UserAgentCheck' => FALSE,
+		'Session_Secure' => TRUE, // check user agent and ip
+		                          
+		// ----------------------| Config |----------------------- #
+		
+		'AppName' => 'Dynamic', // application name (used in cookie name)
+		                        
+		// Whilte List Controllers
+		
+		'Controllers' => array ( 'main' ),
+		
+		// / Method Permissions
+		
+		'CheckPermissions' => TRUE,
+		'ForbiddenByDefault' => TRUE,
+		'AntiCsrf' => TRUE,
+		'CsrfTokenName' => '__pctk',
+		'DefualtPerm' => 'guest' 
 );
