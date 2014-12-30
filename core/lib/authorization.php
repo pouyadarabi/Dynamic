21a4ee7530c7f2310bb31e8ee6c9b9008fff5bd2
pouyadarabi <pouyyadarabi@gmail.com>
2014-12-30 11:22:08 -0800

    add class annotation
    annotations can be set in class for all functions

--- core/lib/authorization.php
+++ core/lib/authorization.php
@@ -4,57 +4,74 @@ use core\main\libs;
 class Authorization extends libs
 {
     public static function DoAuthorization()
-    {
-        $Perms = Session::get('Perms');    
+    {        
         $checkPerm =  $GLOBALS['CONFIG'][ 'CheckPermissions' ];
-        
-        if ( $checkPerm && empty( $Perms ) ){
-        	Session::__setArray( 'Perms', $GLOBALS['CONFIG']['DefualtPerm'] );
-        	$Perms = array($GLOBALS['CONFIG']['DefualtPerm']);
-        }
-      	$REQUEST_METHOD =  strtolower($_SERVER[ 'REQUEST_METHOD' ]);
-        $MethodPerm = Annotations::getMethodAnnotations( '\\app\\controller\\'.__REQ__CLASS__, __REQ__METHOD__ );
-    	if ( !isset( $MethodPerm[ 'ignore' ] ) || !in_array( 'csrf', $MethodPerm[ 'ignore' ] ) ) {
-            if ( isset( $Perms[ 'logined' ] ) && $GLOBALS['CONFIG'][ 'AntiCsrf' ] === TRUE  ) {
-            	$Csrf_Method = explode(',',$GLOBALS['CONFIG'][ 'CsrfCheckMethods' ]);
-                if ( in_array(array('all',$REQUEST_METHOD), $Csrf_Method) ) {
-                    Security::CsrfTokenChecker( $GLOBALS['CONFIG'][ 'CsrfTokenLocation' ],$GLOBALS['CONFIG'][ 'CsrfTokenName' ] );
-                }
-            }
-        }
-      
-        if ( $checkPerm && (!isset( $MethodPerm[ 'perm' ] ) || !is_array( $MethodPerm[ 'perm' ] )) ) {
-            if ( $GLOBALS['CONFIG'][ 'ForbiddenByDefault' ] ) {
-                Helper::RaiseError( 404 , $_SERVER['REQUEST_URI'] );
-            }
-        }
-
-
-        if ( isset( $MethodPerm[ 'method' ] ) && !in_array($REQUEST_METHOD, $MethodPerm[ 'method' ] ) )
-            Helper::RaiseError( 404, $_SERVER['REQUEST_URI'] );
-
-        if($checkPerm){
-	        $MethodPerm = $MethodPerm[ 'perm' ];
-	
-	        if ( $MethodPerm ) {
-	            foreach ( $MethodPerm as $Method ) {
-	
-	                if ( empty( $Perms ) )
-	                    Helper::RaiseError( 404, $_SERVER['REQUEST_URI'] );
-	
-	                if ( in_array( $Method, $Perms ) === TRUE )
-	                    return;
-	            }
-	
-	            Helper::RaiseError( 404 , $_SERVER['REQUEST_URI']);
-	        }
-        }
+      	if ( $checkPerm ){
+      		$Perms = Session::get('Perms');
+      		if ( empty( $Perms ) ){
+      			Session::__setArray( 'Perms', $GLOBALS['CONFIG']['DefualtPerm'] );
+      			$Perms = array($GLOBALS['CONFIG']['DefualtPerm']);
+      		}
+      		
+      		$REQUEST_METHOD =  strtolower($_SERVER[ 'REQUEST_METHOD' ]);
+      		$forbidden = true;
+      		$ClassPerm = Annotations::getClassAnnotations( '\\app\\controller\\'.__REQ__CLASS__ );
+      		$MethodPerm = Annotations::getMethodAnnotations( '\\app\\controller\\'.__REQ__CLASS__, __REQ__METHOD__ );
+      		 
+      		if(isset( $ClassPerm[ 'perm' ] ) && is_array($ClassPerm[ 'perm' ])) {
+      			$forbidden = false;
+      		}
+      			
+      		
+      		
+      		if ( !isset( $MethodPerm[ 'perm' ] ) || !is_array( $MethodPerm[ 'perm' ] ) ) {
+      			if ($forbidden && $GLOBALS['CONFIG'][ 'ForbiddenByDefault' ] ) {
+      				Helper::RaiseError( 404 , $_SERVER['REQUEST_URI'] );
+      			}
+      		}
+      		self::CheckAnnotations($ClassPerm, $Perms, $REQUEST_METHOD);
+      		self::CheckAnnotations($MethodPerm, $Perms, $REQUEST_METHOD);
+      		
+      		
+      		
+      	}
     }
     public static function CheckPermissionByNames($name)
     {
     	$perms = Session::get('Perms');
-    	if (!empty($perms) && in_array( $name, $perms) )
+    	if ( in_array( $name, $perms) )
     		return true;
     	return false;
     }
+
+	private static function CheckAnnotations($array,$Perms,$REQUEST_METHOD){
+		if(!$array)
+			return ;
+		if ( isset( $array[ 'method' ] ) && !in_array($REQUEST_METHOD, $array[ 'method' ] ) )
+			Helper::RaiseError( 404, $_SERVER['REQUEST_URI'] );
+		
+		if ( !isset( $array[ 'ignore' ] ) || !in_array( 'csrf', $array[ 'ignore' ] ) ) {
+			if ( isset( $Perms[ 'logined' ] ) && $GLOBALS['CONFIG'][ 'AntiCsrf' ] === TRUE  ) {
+				$Csrf_Method = explode(',',$GLOBALS['CONFIG'][ 'CsrfCheckMethods' ]);
+				if ( in_array(array('all',$REQUEST_METHOD), $Csrf_Method) ) {
+					Security::CsrfTokenChecker( $GLOBALS['CONFIG'][ 'CsrfTokenLocation' ],$GLOBALS['CONFIG'][ 'CsrfTokenName' ] );
+				}
+			}
+		}
+	
+		$MethodPerm = $array[ 'perm' ];
+		if ( $MethodPerm ) {
+			foreach ( $MethodPerm as $Method ) {
+		
+				if ( empty( $Perms ) )
+					Helper::RaiseError( 404, $_SERVER['REQUEST_URI'] );
+		
+				if ( in_array( $Method, $Perms ) === TRUE )
+					return;
+			}
+		
+			Helper::RaiseError( 404 , $_SERVER['REQUEST_URI']);
+		}
+		
+	}
 } 
