8f69dd8517a8a3c7c52197f96cc822ac6b82fb10
ehsan gholami kenari <EhsanGholami@users.noreply.github.com>
2015-03-18 10:49:24 +0330

    Update authorization.php

--- core/lib/authorization.php
+++ core/lib/authorization.php
@@ -32,10 +32,15 @@ class Authorization extends libs
     public static function CheckPermissionByNames($names)
     {
         $perms = Session::get('Perms');
-        if($perms)
-            foreach ($names as $perm)
-                if ( in_array( $perm, $perms) )
+        if ($perms)
+            if(is_array($names)) {
+                foreach ($names as $perm)
+                    if (in_array($perm, $perms))
+                        return true;
+            }else{
+                if (in_array($names, $perms))
                     return true;
+            }
         return false;
     }
 
