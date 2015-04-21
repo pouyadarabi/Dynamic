f4ccbb9488f44d2b95572f6dc1290c9314c329ae
Pouya Darabi <pouyyadarabi@gmail.com>
2015-04-21 10:02:55 +0430

    Update notfound.php

--- app/controller/notfound.php
+++ app/controller/notfound.php
@@ -6,8 +6,7 @@ use core\lib\Template;
 class NotFound extends Controller {
 
 	public function index() {
-		if (function_exists('http_response_code'))
-	    	http_response_code(404);
+	    http_response_code(404);
 	    Template::set('URL', $_SERVER['REQUEST_URI']);
 	    Template::Show('notfound');
 	}
