ae0ac872084b403ad8eb90ed071731707ba0f98b
Pouya Darabi <pouyyadarabi@gmail.com>
2015-07-20 11:57:40 +0430

    Update controller.php
    optimize functions

--- core/main/controller.php
+++ core/main/controller.php
@@ -7,25 +7,24 @@ class Controller extends AbstractClass
     {
     }
 
-    protected function Redirect( $url = '', $append = '' )
-    { 	
-        if ( trim( $url == '' ) ) {
-        	$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        	$siteURL =  $protocol . $_SERVER[ 'HTTP_HOST' ] . '/' . __Dynamic_PATH__;
-            $url = $siteURL . 'main' . $append;
-        }
-        $url = $this->site_url($url);
-        header( 'location: ' . $url );
-        exit;
-    }
-    protected function site_url( $url = '' )
+    protected function Redirect ($url = '', $append = '')
     {
-    	$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-    	$siteURL =  $protocol . $_SERVER[ 'HTTP_HOST' ] . '/' . __Dynamic_PATH__;
-    	if (parse_url($url, PHP_URL_SCHEME) != '') 
-    		return $url;
-    	$url = str_ireplace(__Dynamic_PATH__, '', $url);
-    	$url = ltrim($url,'/');
-    	return $siteURL . $url;
+        $url  = $this->site_url($url);
+        header('location: ' . $url.$append);
+        exit();
+    }
+
+    protected function site_url ($url = '')
+    {      
+        if (parse_url($url, PHP_URL_SCHEME) != '')
+            return $url;
+        if ($url == ''){
+            $url = __Defualt_Controller__;
+        }
+        $protocol = ((! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        $siteURL = $protocol . $_SERVER['HTTP_HOST'] . '/' . __Dynamic_PATH__;
+        $url = str_ireplace(__Dynamic_PATH__, '', $url);
+        $url = ltrim($url, '/');
+        return $siteURL . $url;
     }
 }
